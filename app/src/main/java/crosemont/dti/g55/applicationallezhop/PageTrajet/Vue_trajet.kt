e118f56880ac380476f3003321f959e43e343e74
Huynh Vincent <2089020@crosemont.qc.ca>
2023-11-02 13:59:45 -0400

    Ajout de message pour indiquer que le boutton reload a ete cliquer

--- app/src/main/java/crosemont/dti/g55/applicationallezhop/PageTrajet/Vue_trajet.kt
+++ app/src/main/java/crosemont/dti/g55/applicationallezhop/PageTrajet/Vue_trajet.kt
@@ -1,18 +1,24 @@
 package crosemont.dti.g55.applicationallezhop.PageTrajet
 
+import android.content.Intent
+import android.content.Intent.getIntent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
-
 import crosemont.dti.g55.applicationallezhop.R
+
 
 /**
  * A simple [Fragment] subclass.
@@ -23,6 +29,9 @@ class vue_trajet : Fragment() {
     lateinit var navController: NavController
     lateinit var btnRéserver : Button
     var présentateurTrajet = PrésentateurTrajet(this)
+    lateinit var reloadButton: ImageButton
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,8 +40,22 @@ class vue_trajet : Fragment() {
         // Déclaration de la vue afin de le lier au layout
         val vue = inflater.inflate(R.layout.fragment_vue_trajet, container, false)
 
+
+        reloadButton = vue.findViewById(R.id.reload)
+        reloadButton.setImageResource(android.R.drawable.ic_popup_sync)
+        reloadButton.setColorFilter(ContextCompat.getColor(requireContext(), android.R.color.black))
+
+
+
+        reloadButton.setOnClickListener {
+            reloadButton()
+        }
         // Inflate the layout for this fragment
         return vue
+
+
+
+
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -77,6 +100,11 @@ class vue_trajet : Fragment() {
 
     fun naviguerVerVueConfirmationRéservation(){
         navController.navigate(R.id.action_vue_trajet_to_vue_confirmation_reservation)
+    }
+
+    fun reloadButton(){
+        Toast.makeText(requireContext(), "Cliqué", Toast.LENGTH_SHORT).show()
+
     }
 
 
