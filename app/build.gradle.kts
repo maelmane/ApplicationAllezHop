4501649aa5a53ae62493006189d4a8cfbd54a5ca
Ralph Christian Gabriel <rgabriel@dept-info.crosemont.quebec>
2023-11-16 16:00:47 -0500

    Modification du build.gradle afin d'implementer les test unitaires pour les presentateurs et modeles de l'application + etape ROUGE pour les test unitaires du Test Driven developpment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,15 +36,24 @@ android {
 }
 
 dependencies {
-
+    // ... (other dependencies)
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
-
+    // Unit Testing
     testImplementation("junit:junit:4.13.2")
+    testImplementation ("org.mockito:mockito-junit-jupiter:5.7.0") // Replace 4.x.x with the actual version
+    testImplementation ("org.mockito:mockito-inline:5.2.0") // For mocking final classes
+
+    // Android Testing
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+    // Kotlin Coroutines Testing
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3") // Replace x.x.x with the latest version
 }
+
