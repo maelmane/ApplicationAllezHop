f763ebe7b7b9cc1ce236ebeade939205a9734414
MMane <mmane@dept-info.crosemont.quebec>
2023-11-16 17:02:44 -0500

    Ajout de google map fonctionnel dans la page d'acceuil

--- app/src/main/java/crosemont/dti/g55/applicationallezhop/PageAcceuil/Vue_accueil.kt
+++ app/src/main/java/crosemont/dti/g55/applicationallezhop/PageAcceuil/Vue_accueil.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
 import crosemont.dti.g55.applicationallezhop.PageAcceuil.PrésentateurAccueil
@@ -18,9 +25,10 @@ import crosemont.dti.g55.applicationallezhop.R
  * Use the [vue_accueil.newInstance] factory method to
  * create an instance of this fragment.
  */
-class vue_accueil : Fragment() {
+class vue_accueil : Fragment(), OnMapReadyCallback {
     lateinit var navController: NavController
     var présentateurAccueil = PrésentateurAccueil(this)
+    lateinit var map : FrameLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +36,10 @@ class vue_accueil : Fragment() {
     ): View? {
         // Déclaration de la vue afin de le lier au layout
         val vue = inflater.inflate(R.layout.fragment_vue_accueil, container, false)
+        map = vue.findViewById(R.id.map)
+        var mapFragment = childFragmentManager.findFragmentById(R.id.map)
+        val supportMapFragment = mapFragment as? SupportMapFragment
+        supportMapFragment?.getMapAsync(this)
         return vue
     }
 
@@ -71,5 +83,27 @@ class vue_accueil : Fragment() {
 
     fun naviguerVerVueTrajet(){
         navController.navigate(R.id.action_vue_accueil_to_vue_trajet)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        // Mettre les coordonnées à Montréal.
+        val montréal = LatLng(45.508888, -73.561668)
+
+        // Mettre le type de la map en hybride.
+        googleMap.mapType = GoogleMap.MAP_TYPE_HYBRID
+
+        // Ajouter un marqueur aux coordonnées
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(montréal)
+                .title("Montréal")
+        )
+
+        // Faire bouger la caméra vers les coordonnées et zoomer
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(montréal))
+        //googleMap.moveCamera(CameraUpdateFactory.zoomTo(10f))
+
+        // Afficher le trafic
+        googleMap.isTrafficEnabled = true
     }
 }
